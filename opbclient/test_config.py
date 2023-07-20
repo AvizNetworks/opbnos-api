f249b034ab0e5b0023533568006fab9e15e8b5b7
vibhinanda-aviz <130724422+vibhinanda-aviz@users.noreply.github.com>
2023-07-19 20:55:04 -0700

    Update test_config.py

--- opbclient/test_config.py
+++ opbclient/test_config.py
@@ -1,9 +1,11 @@
-import pyavizapi_get as avizapi
+import opb_api as api
 
 def main():
 
-    conn = avizapi.connect(host='10.4.4.53',transport='https', user='admin', password='admin', port='8091')
+    conn = api.connect(host='10.4.4.56',transport='https', user='admin', password='admin', port='8091')
     #conn.configFlow()
-    conn.configFlowRules(flow="rule_id_1")
-
+    #conn.configFlow(flow="flow1")
+    #conn.configFlowRules(flow="flow1",r_id="1")
+    #conn.configOverride(flow="flow1",r_id="1",dt = {"override-to": ["Ethernet7_1"],"override-push-vlan-tag": "100","override-pop-vlan": "disable"})
+    conn.configAllInterfaces()
 main()
