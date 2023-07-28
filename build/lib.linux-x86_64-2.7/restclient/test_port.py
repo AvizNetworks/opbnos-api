be80c2438f8a0253e8ce4b2abd4887d5173b3215
Vibhitha Nandakumar <vibhitha.nandakumar@aviznetworks.com>
2023-07-28 00:45:01 -0700

    opbnos code check

---
+++ build/lib.linux-x86_64-2.7/restclient/test_port.py
@@ -0,0 +1,8 @@
+import pyaviz_get
+
+def main():
+    conn = pyaviz_get.connect(host='10.4.4.56',transport='https', user='admin', password='admin', port='8091')
+    #conn.configFlow()
+    conn.configFlowRules(flow="rule_id_1")
+
+main()
