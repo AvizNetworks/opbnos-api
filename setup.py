a96d2f190729c35c601309e074d31b1042d5fc53
Harrish <108512835+sj-harrish@users.noreply.github.com>
2023-10-13 17:02:28 +0530

    Review Comments Addressed

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='opbnos-api',
+    name='opbapi',
     version='0.1',
     packages=find_packages(),
     install_requires=[
       'requests==2.22.0'
       # Add any required dependencies here
     ],
-    author='Vibhitha Nandakumar',
-    description='SDK for OPB-NOS Infrastructure'
-    
+    author='Harrish Jeyabalu, Vibhitha Nandakumar',
+    description='Python SDK to access and manage OPBNOS'
 )
